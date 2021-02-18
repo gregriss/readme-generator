989a4d2f564dcd1fdc04a2e10299823f937fe709
Greg Riss <gregriss23@gmail.com>
2021-02-17 21:50:46 -0500

    requiring fs and inquirer, created UserQuestion constructor and askInitialQuestions inquirer function with preliminary question objects

--- develop/index.js
+++ develop/index.js
@@ -1,7 +1,50 @@
 // TODO: Include packages needed for this application
-
+const fs = require('fs');
+const inquirer = require('inquirer');
 // TODO: Create an array of questions for user input
 const questions = [];
+function UserQuestion(question){
+    this.message = question;
+    this.type = "input";
+    this.name = dataname;
+}
+
+function askInitialQuestions(){
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "",
+            name: "q1"
+        },
+        {
+            type: "input",
+            message: "",
+            name: "col1"
+        },
+        {
+            type: "input",
+            mesasge: "",
+            name: "q2"
+        },
+        {
+            type: "input",
+            message: "",
+            name: "col2"
+        }
+
+    ]).then( responses => {
+        const {q1, col1, q2, col2} = responses;
+        console.log(responses);
+        columnout = `"${col1}", "${col2}"\n`;
+        console.log(columnout);
+
+        let userQuestionsArr = [];
+
+        userQuestionsArr.push(new UserQuestion(q1, "data1"));
+        userQuestionsArr.push(new UserQuestion(q2, "data2"));
+    });
+}
+askInitialQuestions();
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
